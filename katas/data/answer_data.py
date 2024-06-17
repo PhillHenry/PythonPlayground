a05b0711e6de3adfe0f66411eded389bc1d5f4cf
PhillipHenry <PhillHenry@gmail.com>
2024-06-17 17:12:12 +0100

    None ambiguous. 71 not mapped

--- katas/data/answer_data.py
+++ katas/data/answer_data.py
@@ -14,13 +14,20 @@ def drop_last_underscore(x: str) -> str:
     matched = None
     if match:
         matched = match.group(1).strip()
-    else:
-        print(f"Don't know how to parse '{x.strip()}'")
+    return matched
+
+
+def drop_version(x: str) -> str:
+    match = re.match(r'^(.*)_\d+$', x.strip())
+    matched = None
+    if match:
+        matched = match.group(1).strip()
     return matched
 
 
 class Mismatch:
-    def __init__(self, num_matches: int, message: str):
+    def __init__(self, num_matches: int, message: str, schema: str):
+        self.schema = schema
         self.message = message
         self.num_matches = num_matches
     def __repr__(self):
@@ -29,11 +36,17 @@ class Mismatch:
 
 
 def try_match(obj: str, mismathes: dict, df: pd.DataFrame, schema: str) -> str:
-    table = drop_last_underscore(obj)
+    obj = obj.strip()
+    table = drop_version(obj)
     if not table:
         table = obj.strip()
 
     warehouse = df[(df[VIEW].str.lower() == table.lower()) & (df[SCHEMA].str.lower() == schema.lower())]
+    if len(warehouse) != 1:
+        dropped = drop_last_underscore(table)
+        if dropped:
+            table = dropped
+        warehouse = fuzzy_match(df, schema, table)
     num_match = len(warehouse)
     if num_match != 1 and len(warehouse.groupby([SCHEMA, VIEW]).size().reset_index(name='count')) != 1:
         if num_match > 1:
@@ -42,12 +55,19 @@ def try_match(obj: str, mismathes: dict, df: pd.DataFrame, schema: str) -> str:
             others = ""
         msg = f"""Table '{table}' in {schema} ({obj.strip()}) matches {num_match} rows in warehouse
 {others}"""
-        mismathes[table] = Mismatch(num_match, msg)
+        mismathes[table] = Mismatch(num_match, msg, schema)
 
     return table.lower()
 
 
-if __name__ == "__main__":
+def fuzzy_match(df, schema, table):
+    mask = (df[VIEW].apply(lambda x: table.lower().endswith(x.lower()))) & (
+                df[SCHEMA].str.lower() == schema.lower())
+    warehouse = df[mask]
+    return warehouse
+
+
+def do_match():
     with open("/home/henryp/Documents/Answer/29946/schema_object.csv", "r") as f:
         lines = f.readlines()
     print(len(lines))
@@ -73,6 +93,11 @@ if __name__ == "__main__":
             print(f"{table}: {mismatch}")
             count += 1
     print(f"{count} not mapped")
-    assert(len(matching) == set(tables))
+    print(f"The schemas of those not mapped are {set([miss.schema for miss in mismathes.values() if miss.num_matches == 0])}")
+    assert (len(matching) == set(tables))
+
+
+if __name__ == "__main__":
+    do_match()
 
 
