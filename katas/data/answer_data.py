86e100cbf3ef6545583e277934b4e59d59ee648d
PhillipHenry <PhillHenry@gmail.com>
2024-06-17 16:05:06 +0100

    Missing some.

--- katas/data/answer_data.py
+++ katas/data/answer_data.py
@@ -1,6 +1,8 @@
 import re
 import pandas as pd
 
+VIEW = "View"
+
 
 def read_warehouse() -> pd.DataFrame:
     df = pd.read_csv("/home/henryp/Documents/Answer/Warehouse.csv", header=0)
@@ -20,18 +22,24 @@ if __name__ == "__main__":
         lines = f.readlines()
     print(len(lines))
     df = read_warehouse()
-    tables = set([])
+    tables = []
     for line in lines:
         schema, obj = line.split(",")
         table = drop_last_underscore(obj)
         if not table:
             table = obj.strip()
-        tables.update(table)
-        warehouse = df[df["View"].str.lower() == "Commissioner_Hierarchies".lower()]
-        if table.strip() == "SUS+ Faster Emergency Care Data Set (ECDS) (Daily)":
-            print(warehouse)
-        if len(warehouse) != 1:
-            print(f"Could not match {table}")
-            print(warehouse)
+        tables.append(table.lower())
+        warehouse = df[df[VIEW].str.lower() == table.lower()]
+        num_match = len(warehouse)
+        if num_match != 1:
+            print(f"Table '{table}' ({obj.strip()}) matches {num_match} rows in warehouse")
+            if num_match > 1:
+                print(warehouse[VIEW])
+    assert len(tables) == len(lines)
+    tables = set(tables)
+    matching = df[df[VIEW].str.lower().isin(tables)]
+    print(len(matching))
+    print(f"len(matching = {len(matching)}, set of names = {len(tables)}")
+    assert(len(matching) == set(tables))
 
 
